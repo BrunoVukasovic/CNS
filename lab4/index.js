8e4107a575889dc576dbac4fd9ce5d0c23903477
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2019-05-16 18:00:38 +0200

    lab4 moved

--- lab4/index.js
+++ lab4/index.js
@@ -3,37 +3,36 @@ const axios = require("axios");
 let challengeCiphertext = "";
 let length = null;
 
-// get ciphertext that needs to be decrypted
-// server reponds with ciphertext and iv used in CBC
-async function GetChallenge() {
-  const response = await axios.get("http://localhost:3000/ctr/challenge");
-  challengeCiphertext = response.data.ciphertext;
-  length = challengeCiphertext.length;
-  console.log("challengeCiphertext: " + challengeCiphertext);
-  console.log("length: " + cipherLength);
-}
-// GetChallenge();
+async function Main() {
+  // dohvaćanje challenge
+  const challenge = await axios.get("http://10.0.15.6/ctr/challenge");
+  challengeCiphertext = challenge.data.ciphertext;
 
-async function PostPlaintext() {
-  challengeCiphertext =
-    "b4420e24f1f57ee0bce3f5835a0b26b176f3d33375361ba15c493c69c944c8004bacc1b01cf102acc1552d62e3";
+  // jer je challengeCiphertext u hex formatu
+  // 2 hex znaka = 1 ascii
   length = challengeCiphertext.length / 2;
 
+  // generiranje random rijeci duljine length u hex zapisu
+  // chosen-plaintext mora bit iste duljine kao onaj koji zelimo otkrit
   const wordHex = generate_random_hex_string(length);
 
   while (1) {
-    const response = await axios.post("http://localhost:3000/ctr", {
+    const response = await axios.post("http://10.0.15.6/ctr", {
       plaintext: wordHex
     });
     let cipher = response.data.ciphertext;
+
+    // xor priprema
     cipher = Buffer.from(cipher, "hex");
     challengeCiphertext = Buffer.from(challengeCiphertext, "hex");
     let word = Buffer.from(wordHex, "hex");
     xorResult = Buffer.alloc(length);
 
+    // xor
     for (let i = 0; i < cipher.length; i++) {
       xorResult[i] = challengeCiphertext[i] ^ cipher[i] ^ word[i];
     }
+
     xorResult = xorResult.toString("utf8");
     if (xorResult.search("Chuck") != -1 || xorResult.search("Norris") != -1) {
       console.log(xorResult);
@@ -41,7 +40,7 @@ async function PostPlaintext() {
     }
   }
 }
-PostPlaintext();
+Main();
 
 function generate_random_hex_string(string_length) {
   let random_string = "";
